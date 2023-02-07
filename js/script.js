d5c161a9f4c8ed22208f9dc81073cf54c408b6f3
Kaua William Araujo Ribeiro <886869@senacminas.edu.br>
2023-02-07 17:36:53 -0300

    Criação de animação ao scroll e animação ao redor do site

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-const homeUl = document.querySelectorAll(".home-img");
-const homeImg = document.querySelectorAll(".home-img li");
-function handleClick(event) {
-  homeImg.forEach((item) => {
-    item.classList.toggle("ativo");
+const dataScroll = document.querySelectorAll('[data-anime="scroll"]');
+
+function handleScroll() {
+  dataScroll.forEach((item) => {
+    const windowHalf = window.innerHeight * 0.6;
+    const itemTop = item.getBoundingClientRect().top;
+    const validation = windowHalf - itemTop > 0;
+    if (validation) {
+      item.classList.add("ativo");
+    } else {
+      item.classList.remove("ativo");
+    }
   });
 }
-homeUl[0].addEventListener("click", handleClick);
+
+window.addEventListener("scroll", handleScroll);
+
+const homeUl = document.querySelectorAll(".home-img");
+const homeLi = document.querySelectorAll(".home-img li");
+
+function handleClick() {
+  const homeC = homeLi[0].classList.contains("back");
+  if (!homeC) {
+    homeLi[0].nextElementSibling.classList.add("front");
+    homeLi[0].classList.add("back");
+  } else {
+    homeLi[0].nextElementSibling.classList.remove("front");
+    homeLi[0].classList.remove("back");
+  }
+}
+
+addEventListener("click", handleClick);
